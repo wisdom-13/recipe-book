13e72fc234199aa27a79edd21208600e93cc91e1
jhlee <wisdom_13_@naver.com>
2023-02-02 15:21:48 +0900

    router 설정

--- src/index.js
+++ src/index.js
@@ -1,17 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import NotFound from './pages/NotFound';
+import Main from './pages/Main';
+import AllRecipe from './pages/AllRecipe';
+import RecipeBook from './pages/RecipeBook';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <NotFound />,
+    children: [
+      { index: true, path: '/', element: <Main /> },
+      { path: '/recipe', element: <AllRecipe /> },
+      {
+        path: '/recipe/:id',
+        element: <RecipeBook />,
+      },
+    ]
+  }
+]);
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
---
+++ src/pages/AllRecipe.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function AllRecipe() {
+  return (
+    <div>
+      AllRecipe
+    </div>
+  );
+}
+
