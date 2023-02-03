6ac1fcc6be56f358aaf5ef0c5ee0802c827eec9f
jhlee <wisdom_13_@naver.com>
2023-02-03 21:38:55 +0900

    navbar 추가

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Outlet } from 'react-router';
+import Navbar from './component/Navbar';
 
 export default function App() {
   return (
     <div>
+      <Navbar />
       <Outlet />
     </div>
   );
