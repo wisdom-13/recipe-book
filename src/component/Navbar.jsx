a04013eb1a950064bf395b1506123151282fcd08
jhlee <wisdom_13_@naver.com>
2023-02-04 19:48:54 +0900

    navbar 내용 추가

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Navbar() {
   return (
-    <div>
-      navbar
-    </div>
+    <header className='flex justify-between'>
+      <Link to='/'>
+        <h1>냉장고 쏙 레시피</h1>
+      </Link>
+      <nav className='flex gap-4'>
+        <Link to='/recipe'>레시피</Link>
+        <Link to='/'>냉장고</Link>
+      </nav>
+    </header>
   );
 }
 
